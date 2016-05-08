ee7e49a9c321697d43d7bd28885dee1022a3fbf4
BuildTools <unconfigured@null.spigotmc.org>
2016-05-08 16:48:11 +0200

    Further visual improvements.

--- public/js/app/display.js
+++ public/js/app/display.js
@@ -18,6 +18,9 @@ APP.display.registerEvents = function() {
 		APP.display.showBetScreen();
 		APP.game.loadUserInventory(APP.display.addBetScreenItems);
 	});
+	$('.deposit-footer a').click(function() {
+		APP.display.hideBetScreen();
+	});
 };
 
 APP.display.setupHistoryBar = function() {
@@ -67,14 +70,13 @@ APP.display.showBetScreen = function() {
 		html +='<div class="item-box"><p class="item-title">AK-47 | Redline</p><img class="item-image" src="http://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJemkV09-5lpKKqPrxN7LEm1Rd6dd2j6eQ9N2t2wK3-ENsZ23wcIKRdQE2NwyD_FK_kLq9gJDu7p_KyyRr7nNw-z-DyIFJbNUz/80fx60f"><p class="item-info">FT | $5.49</p></div>'
 		html += '</div><div class="deposit-footer"><a>Cancel</a><button type="submit" class="btn btn-primary" disabled>Add to deposit...</button></div>';
 		$('.jackpot-deposit').html($('.jackpot-deposit').html() + html);
-		$('.deposit-footer a').click(function() {
-			APP.display.hideBetScreen();
-		});
 		$('.item-box').click(function() {
 			$(this).toggleClass('checked');
 		});
 		$('.jackpot-deposit').children().not('.add-items').hide();
 		$('.jackpot-deposit').children().not('.add-items').fadeIn(300);
+
+		APP.display.registerEvents();
 	}, 500);
 };
 
@@ -89,11 +91,11 @@ APP.display.hideBetScreen = function() {
 	$('.jackpot-info').css('margin-top', '40px');
 	$('.jackpot-listing').css('margin-top', '30px');
 	$('.jackpot-deposit').css({
-		'-webkit-transition': 'min-height 1.2s',
-		'-moz-transition': 'min-height 1.2s',
-		'-o-transition': 'min-height 1.2s',
-		'-ms-transition': 'min-height 1.2s',
-		'transition': 'min-height 1.2s',
+		'-webkit-transition': 'min-height 1s',
+		'-moz-transition': 'min-height 1s',
+		'-o-transition': 'min-height 1s',
+		'-ms-transition': 'min-height 1s',
+		'transition': 'min-height 1s',
 		'min-height': '0px'
 	});
 	setTimeout(function() {
@@ -105,13 +107,11 @@ APP.display.hideBetScreen = function() {
 			'-ms-transition': '',
 			'transition': ''
 		});
-		$('.add-items a').click(function() {
-			APP.display.showBetScreen();
-			APP.game.loadUserInventory(APP.display.addBetScreenItems);
-		});
 		$('.main-container').css({
 			'overflow-y': 'visible'
 		});
+
+		APP.display.registerEvents();
 	}, 100);
 };
 
